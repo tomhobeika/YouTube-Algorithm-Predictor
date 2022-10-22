27b9783f9f894fdd754685ededc1d7a250ad7125
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-10-23 02:32:09 +1100

    Update testModel.py

--- testModel.py
+++ testModel.py
@@ -1,75 +1,103 @@
 from tensorflow import keras
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import numpy as np
 from PIL import Image
 import os
 
+# Downscale thumbnails to 360p for training
+img_size = (480, 360)
+
 def loadImage(path):
-	# Read image and resize to 480p
-	img = Image.open(path, 'r').convert('RGB').resize((640, 480))
+	img = Image.open(path, 'r').convert('RGB').resize(img_size)
 	# Convert colors from 0-255 to 0-1
 	return np.asarray(img) / 255
 
-# AI will try to match target output given source as input
+# AI tries to match target values given source values
 source = []
 target = []
-ids = []
 
-# Import thumbs from /dataset/ folder
+# Import thumbnails from dataset/ folder
 for img in os.listdir('dataset'):
 	source.append(loadImage(f'dataset/{img}'))
-	ids.append(img[:11])
-	views = int(img[12:].split("_")[0])
-	target.append(views)
+	views, subs = img[12:-4].split("_")
+	target.append(int(views) / int(subs))
 
-# Convert stuff to numpy arrays
+# Convert to numpy arrays
 source = np.array(source)
 target = np.array(target)
 
-# Create basic sequential model
+# This model is really struggling lmao
 model = keras.models.Sequential(
 	[
-		# Input is 640 x 480 x 3 color channels (RGB)
-		keras.layers.Conv2D(filters=32, kernel_size=5, strides=2, padding='Same', activation='relu', input_shape=(480, 640, 3)),
-		keras.layers.MaxPooling2D(pool_size=2),
+		# Input has 3 dimensions: height x width x 3 color channels (RGB)
+		keras.Input(shape=(img_size[1], img_size[0], 3)),
 
+		# Try to learn small-scale details + shrink input
+		keras.layers.Conv2D(filters=8, kernel_size=3, strides=2, padding='Same', activation='relu'),
+		keras.layers.Conv2D(filters=16, kernel_size=3, strides=2, padding='Same', activation='relu'),
+		keras.layers.MaxPooling2D(pool_size=2, strides=2),
+
+		# Try to learn medium-scale details + shrink input
+		keras.layers.Conv2D(filters=32, kernel_size=3, strides=2, padding='Same', activation='relu'),
+		keras.layers.Conv2D(filters=32, kernel_size=3, strides=2, padding='Same', activation='relu'),
+		keras.layers.MaxPooling2D(pool_size=2, strides=2),
+
+		# Try to learn large-scale details + shrink input
+		keras.layers.Conv2D(filters=64, kernel_size=3, strides=2, padding='Same', activation='relu'),
 		keras.layers.Conv2D(filters=64, kernel_size=3, strides=2, padding='Same', activation='relu'),
 		keras.layers.MaxPooling2D(pool_size=2, strides=2),
 
-		keras.layers.Conv2D(filters=96, kernel_size=3, strides=2, padding='Same', activation='relu'),
-		keras.layers.MaxPooling2D(pool_size=2, strides=2),
-
+		# Convert from 3D to 1D
 		keras.layers.Flatten(),
-		keras.layers.Dense(16384),
-		keras.layers.Dense(4096),
+
+		# Hope neuron layers do something useful, may blow up your GPU
+		keras.layers.Dense(16384, activation='relu'), # idk whether to activate this
+		keras.layers.Dense(4096), # idk whether to activate this
+
+		# Output has 1 dimension: view count / subscriber count
 		keras.layers.Dense(1)
-		# Output is 1D view count
 	]
 )
 
+# 'mae' seems slightly better than 'mse'
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss='mae')
 model.summary()
 
-batch_size = 64
-epochs = 128
+batch_size = 128
+epochs = 64
 
-model.fit(source, target,
+x_train, x_valid, y_train, y_valid = train_test_split(source, target, test_size=0.1)
+
+history = model.fit(x_train, y_train,
 	batch_size=batch_size,
 	epochs=epochs,
-	steps_per_epoch=source.shape[0] // batch_size # num_samples / batch_size
+	validation_data=(x_valid, y_valid),
+	steps_per_epoch=x_train.shape[0] // batch_size # num_samples / batch_size
 )
 
-preds = model.predict(source).flatten()
+# Plot model loss
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
+plt.title("Model Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(["train", "validation"])
+plt.show()
 
-# Plot results
-num_results = 10
+# Predict all validation thumbnails
+preds = model.predict(x_valid).flatten()
+
+# Plot a couple of predictions
+num_results = 32
 width = 0.35
 fig, ax = plt.subplots()
 x = np.arange(num_results)
-rects1 = ax.bar(x - width / 2, preds[:num_results], width, label='Predicted')
-rects2 = ax.bar(x + width / 2, target[:num_results], width, label='Actual')
+rects1 = ax.bar(x - width / 2, y_valid[:num_results], width, label='Actual')
+rects2 = ax.bar(x + width / 2, preds[:num_results], width, label='Predicted')
 ax.legend()
 ax.bar_label(rects1, padding=3)
 ax.bar_label(rects2, padding=3)
 fig.tight_layout()
+plt.title("Actual vs predicted views")
 plt.show()
